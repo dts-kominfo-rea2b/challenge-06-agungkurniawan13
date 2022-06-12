f9b9280873be7337997ae901469eb6f22d02cca1
Agung Kurniawan <agung.kurniawan1894@gmail.com>
2022-06-12 14:48:54 +0700

    hallenge 06 Agung Kurniawan (152235865101-374)

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,54 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const dataProcessing = (dataset) => {
+  const data = JSON.parse(dataset);
+  let expWord = "";
+
+  if (data?.message !== undefined) {
+    expWord = data?.message;
+  }
+
+  if (data?.length) {
+    data?.forEach((item) => {
+      if (item?.message !== undefined) {
+        expWord = item?.message;
+      }
+
+      if (item?.data?.message !== undefined) {
+        expWord = item?.data?.message;
+      }
+    });
+  }
+
+  let expWordString = "";
+  const splittedWord = expWord.split(" ");
+
+  if (splittedWord?.length >= 1) {
+    expWordString = splittedWord[1];
+  }
+
+  return expWordString;
+};
+
+const bacaData = (fnCallback) => {
+  const fileList = [file1, file2, file3];
+  let result = [];
+  let countList=0;
+  
+  fileList.forEach((item) => {
+    fs.readFile(item, (error, data) => {
+      countList++;
+      if (error) fnCallback(error, null);
+      const getProcessedItem = dataProcessing(data);
+      result.push(getProcessedItem);
+      if(countList===fileList.length) {
+        fnCallback(null, result);
+      }
+    });
+  });
+  
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
